87cfbc2a7842c5e7ce4c663097824522432aa7e8
NervousOrange <zch693922442@outlook.com>
2020-01-14 21:08:50 +0100

    create a new project Multi-Thread-Crawler

---
+++ src/main/java/com/github/NervousOrange/Main.java
@@ -0,0 +1,14 @@
+package com.github.NervousOrange;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("");
+        for (int i = 0; i < 10; i++ ) {
+            System.out.println("1");
+        }
+
+    }
+}
+
+
