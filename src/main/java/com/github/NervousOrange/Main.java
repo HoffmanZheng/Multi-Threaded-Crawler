16a363b86e2e490096e3bd6510406fbaec347285
NervousOrange <zch693922442@outlook.com>
2020-01-16 20:20:27 +0100

    为爬虫算法构建本地数据库
    - 链接池、已处理链接均从本地数据库读取
    - 对爬取的新闻内容页，直接将网页的标题、内容、URL写入数据库
    - 实现断点续传（但第一次爬取须爬完新浪手机首页，
      这样才能将爬取的链接存入链接池）

--- src/main/java/com/github/NervousOrange/Main.java
+++ src/main/java/com/github/NervousOrange/Main.java
@@ -13,13 +13,11 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.stream.Collectors;
 
 public class Main {
-    static HashSet<String> newsLinkPool = new HashSet<>();
-    static HashSet<String> processedLink;
     DatabaseOperation databaseOperation;
 
     public static void main(String[] args) {
@@ -29,34 +27,43 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
     public void run() throws IOException {
         String databaseURL = "jdbc:h2:file:C:/Users/zch69/recipes/Multi-Threaded-Crawler/CrawlerDatabase";
         databaseOperation = new DatabaseOperation(databaseURL);
         databaseOperation.connectDatabase();
-
-        while (true) {  // 循环，每次都重新从数据库加载
-            ArrayList<String> linkPool = databaseOperation.loadLinkFromDatabase("select LINK from LINKS_TO_BE_PROCESSED");
-            processedLink = new HashSet<>(databaseOperation.loadLinkFromDatabase("select LINK from LINKS_ALREADY_PROCESSED"));
-            while (!linkPool.isEmpty()) {
-                String link = linkPool.remove(linkPool.size() - 1);
-                databaseOperation.updateDatabase("DELETE FROM LINKS_TO_BE_PROCESSED WHERE LINK = ?", link);
-                if (isInterestLink(link) && !isLinkAlreadyProcessed(link)) {
-                    databaseOperation.updateDatabase("insert into LINKS_ALREADY_PROCESSED (link) values(?)", link);
-                    try (CloseableHttpResponse response1 = getHttpResponse(link)) {
-                        HttpEntity entity1 = printStatusLineAndGetEntity(response1);
-                        Document doc = parseEntity(entity1);
-                        linkPool.addAll(getLinkOnThePage(doc));
-                        EntityUtils.consume(entity1);
+        ArrayList<String> linkPool;
+        initialDatabaseFirstTime();
+        while (!(linkPool = databaseOperation.loadLinkFromDatabase("select LINK from LINKS_TO_BE_PROCESSED")).isEmpty()) {  // 循环，每次都重新从数据库加载
+            String link = linkPool.remove(linkPool.size() - 1);
+            databaseOperation.updateLinkInDatabase("DELETE FROM LINKS_TO_BE_PROCESSED WHERE LINK = ?", link);
+            if (isInterestLink(link) && isLinkNotBeProcessedYet(link)) {
+                databaseOperation.updateLinkInDatabase("insert into LINKS_ALREADY_PROCESSED (link) values(?)", link);
+                try (CloseableHttpResponse response1 = getHttpResponse(link)) {
+                    HttpEntity entity1 = printStatusLineAndGetEntity(response1);
+                    Document doc = parseEntity(entity1);
+                    for (String newsLink : getLinkOnThePage(doc)) {
+                        databaseOperation.updateLinkInDatabase("INSERT INTO LINKS_TO_BE_PROCESSED (link) values (?)", newsLink);
                     }
+                    EntityUtils.consume(entity1);
                 }
             }
         }
     }
 
-    public static ArrayList<String> getLinkOnThePage(Document doc) {
+    public void initialDatabaseFirstTime() {
+        ArrayList<String> linkPool = databaseOperation.loadLinkFromDatabase("select LINK from LINKS_TO_BE_PROCESSED");
+        if (linkPool.isEmpty() && isLinkNotBeProcessedYet("http://sina.cn")) {
+            try {
+                databaseOperation.initializeDatabase();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public ArrayList<String> getLinkOnThePage(Document doc) {
         ArrayList<String> linkFromPage = new ArrayList<>();
         ArrayList<Element> aTags = doc.select("a");
         for (Element aTag : aTags) {
@@ -64,15 +71,14 @@ public class Main {
             if (isANewsLink(tempLink)) {
                 System.out.println("A News Link: " + tempLink);
                 getNewsTitleAndContent(tempLink);
-                newsLinkPool.add(tempLink);
-            } else if (excludeUselessLink(tempLink)) {
+            } else if (isLinkUseful(tempLink)) {
                 linkFromPage.add(tempLink);
             }
         }
         return linkFromPage;
     }
 
-    public static void getNewsTitleAndContent(String newsLink) {
+    public void getNewsTitleAndContent(String newsLink) {
         try (CloseableHttpResponse response1 = getHttpResponse(newsLink)) {
             HttpEntity entity1 = printStatusLineAndGetEntity(response1);
             Document doc = parseEntity(entity1);
@@ -82,6 +88,7 @@ public class Main {
                 String title = doc.select("title").text();
                 Elements paragraphs = doc.select("p");
                 String content = paragraphs.stream().map(Element::text).collect(Collectors.joining("\n"));
+                databaseOperation.writeNewsPagesIntoDatabase("INSERT INTO NEWS (TITLE, CONTENT, URL) VALUES (?, ?, ?)", title, content, newsLink);
                 System.out.println(title);
             }
             EntityUtils.consume(entity1);
@@ -90,20 +97,18 @@ public class Main {
         }
     }
 
-    public boolean isLinkAlreadyProcessed(String link) {
-        return databaseOperation.isLinkInDatabase("SELECT LINK FROM LINKS_ALREADY_PROCESSED WHERE LINK = ?", link);
-    }
-
-    public static void specialHandleWithGamesLink(String gamesLink) {
+    public void specialHandleWithGamesLink(String gamesLink) {
         Document gamesDoc = null;
         try {
             gamesDoc = Jsoup.parse(new URL(gamesLink).openStream(), "UTF-8", gamesLink);
         } catch (IOException e) {
             e.printStackTrace();
         }
+        assert gamesDoc != null;
         String title = gamesDoc.select("title").text();
         Elements paragraphs = gamesDoc.select("p");
         String content = paragraphs.stream().map(Element::text).collect(Collectors.joining("\n"));
+        databaseOperation.writeNewsPagesIntoDatabase("INSERT INTO NEWS (TITLE, CONTENT, URL) VALUES (?, ?, ?)", title, content, gamesLink);
         System.out.println(title);
     }
 
@@ -117,17 +122,20 @@ public class Main {
     }
 
     private static boolean isInterestLink(String link) {
-        return link.contains("sina.cn") && (!link.contains("reload")) && (!processedLink.contains(link)) && (!link.contains("tousu"));
+        return link.contains("sina.cn") && (!link.contains("reload")) && (!link.contains("tousu"));
     }
 
     private static boolean isANewsLink(String tempLink) {
         return (tempLink.contains("detail") || tempLink.contains("article")) && !tempLink.contains("video.sina.cn");
     }
 
-    // 不是新闻内容页，加入链接池，排除异常链接：包含 passport、php?、mail、live、排除已经处理过的链接
-    private static boolean excludeUselessLink(String tempLink) {
+    private static boolean isLinkUseful(String tempLink) {
         return !tempLink.contains("javascript") && !tempLink.contains("void") && !(tempLink.length() < 2)
                 && !tempLink.contains("php?") && !tempLink.contains("live") && !tempLink.contains("passport.sina.cn");
+    }
+
+    public boolean isLinkNotBeProcessedYet(String link) {
+        return !databaseOperation.isLinkInDatabase("SELECT LINK FROM LINKS_ALREADY_PROCESSED WHERE LINK = ?", link);
     }
 
     private static HttpEntity printStatusLineAndGetEntity(CloseableHttpResponse response1) {
