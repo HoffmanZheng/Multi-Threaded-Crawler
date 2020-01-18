e4dd27eb4cae52673ffe223a2f3cd46333985e1f
NervousOrange <zch693922442@outlook.com>
2020-01-18 16:45:41 +0100

    使用 MyBatis 简化数据库操作
    - 将数据库语句先从爬虫类中分离出来到DAO类
    - 引入 MyBatis，建立数据库语句的对象关系映射 ORM
    - 创建 MyBatisCrawlerDAO,利用ORM 重现所有的数据库操作
    - 对相似的数据库操作，使用动态 sql 语言简化。
    - 创建 News 类，便于在 MyBatis 中映射参数。

--- src/main/java/com/github/NervousOrange/DatabaseAccessObject.java
+++ src/main/java/com/github/NervousOrange/DatabaseAccessObject.java
@@ -3,19 +3,33 @@ package com.github.NervousOrange;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import java.sql.*;
-import java.util.ArrayList;
 
-public class DatabaseAccessObject {
+public class DatabaseAccessObject implements CrawlerDAO {
     Connection connection;
-    String databaseURL = "jdbc:h2:file:C:/Users/zch69/recipes/Multi-Threaded-Crawler/CrawlerDatabase";
+    static final String databaseURL = "jdbc:h2:file:C:/Users/zch69/recipes/Multi-Threaded-Crawler/CrawlerDatabase";
 
-    public ArrayList<String> loadLinkFromDatabase(String sqlStatement) {
+    public DatabaseAccessObject() {
+        connectDatabase();
+    }
+
+
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
+    public void connectDatabase() {
+        try {
+            connection = DriverManager.getConnection(databaseURL, "root", "root");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String loadLinkFromDatabase() {
         ResultSet resultSet = null;
-        ArrayList<String> linksFromDatabase = new ArrayList<>();
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlStatement)) {
+        String linkToBeProcessed = null;
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT LINK FROM LINKS_TO_BE_PROCESSED LIMIT 1")) {
             resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
-                linksFromDatabase.add(resultSet.getString(1));
+                linkToBeProcessed = (resultSet.getString(1));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -28,12 +42,12 @@ public class DatabaseAccessObject {
                 }
             }
         }
-        return linksFromDatabase;
+        return linkToBeProcessed;
     }
 
-    // insert or delete a link
-    public void updateLinkInDatabase(String sqlStatement, String link) {
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlStatement)) {
+    @Override
+    public void insertLinkInLinkToBeProcessed(String link) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO LINKS_TO_BE_PROCESSED (link) values (?)")) {
             preparedStatement.setString(1, link);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
@@ -41,8 +55,29 @@ public class DatabaseAccessObject {
         }
     }
 
-    public void writeNewsPagesIntoDatabase(String sqlStatement, String title, String content, String URL) {
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlStatement)) {
+    @Override
+    public void insertLinkInLinkAlreadyProcessed(String link) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("insert into LINKS_ALREADY_PROCESSED (link) values(?)")) {
+            preparedStatement.setString(1, link);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void deleteLinkInDatabase(String link) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM LINKS_TO_BE_PROCESSED WHERE LINK = ?")) {
+            preparedStatement.setString(1, link);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void writeNewsPagesIntoDatabase(String title, String content, String URL) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO NEWS (TITLE, CONTENT, URL, CREATED_AT, MODIFIED_AT) VALUES (?, ?, ?, now(), now())")) {
             preparedStatement.setString(1, title);
             preparedStatement.setString(2, content);
             preparedStatement.setString(3, URL);
@@ -52,9 +87,10 @@ public class DatabaseAccessObject {
         }
     }
 
-    public boolean isLinkInDatabase(String sqlStatement, String link) {
+    @Override
+    public boolean isLinkInDatabase(String link) {
         ResultSet resultSet = null;
-        try (PreparedStatement preparedStatement = connection.prepareStatement(sqlStatement)) {
+        try (PreparedStatement preparedStatement = connection.prepareStatement("SELECT LINK FROM LINKS_ALREADY_PROCESSED WHERE LINK = ?")) {
             preparedStatement.setString(1, link);
             resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
@@ -74,12 +110,4 @@ public class DatabaseAccessObject {
         return false;
     }
 
-    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
-    public void connectDatabase() {
-        try {
-            connection = DriverManager.getConnection(this.databaseURL, "root", "root");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
 }
---
+++ src/main/java/com/github/NervousOrange/MyBatisCrawlerDAO.java
@@ -0,0 +1,78 @@
+package com.github.NervousOrange;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+
+public class MyBatisCrawlerDAO implements CrawlerDAO {
+    private SqlSessionFactory sqlSessionFactory;
+
+    public MyBatisCrawlerDAO() {
+        String resource = "db/MyBatis/config.xml";
+        InputStream inputStream;
+        {
+            try {
+                inputStream = Resources.getResourceAsStream(resource);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+
+    @Override
+    public String loadLinkFromDatabase() {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            return (String) session.selectOne("com.github.NervousOrange.CrawlerMapper.loadLinkFromDatabase");
+        }
+    }
+
+    @Override
+    public void insertLinkInLinkToBeProcessed(String link) {
+        insertLinkIntoDatabase(link, "LINKS_TO_BE_PROCESSED");
+    }
+
+    @Override
+    public void insertLinkInLinkAlreadyProcessed(String link) {
+        insertLinkIntoDatabase(link, "LINKS_ALREADY_PROCESSED");
+    }
+
+    private void insertLinkIntoDatabase(String link, String tableName) {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            HashMap param = new HashMap();
+            param.put("tableName", tableName);
+            param.put("link", link);
+            session.selectOne("com.github.NervousOrange.CrawlerMapper.insertLinkIntoDatabase", param);
+        }
+    }
+
+
+    @Override
+    public void deleteLinkInDatabase(String link) {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            session.selectOne("com.github.NervousOrange.CrawlerMapper.deleteLINKS_TO_BE_PROCESSED", link);
+        }
+    }
+
+
+    @Override
+    public void writeNewsPagesIntoDatabase(String title, String content, String URL) {
+        try (SqlSession session = sqlSessionFactory.openSession(true)) {
+            session.selectOne("com.github.NervousOrange.CrawlerMapper.insertNews", new News(title, content, URL));
+        }
+    }
+
+    @Override
+    public boolean isLinkInDatabase(String link) {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            int count = session.selectOne("com.github.NervousOrange.CrawlerMapper.isLinkInDatabase", link);
+            return count != 0;
+        }
+    }
+}
